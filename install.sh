e823ffb7ef714c909f0d6d9ca45cc3603616b96c
gnysearat <ptrsdwski@gmail.com>
2020-10-06 15:50:54 -0700

    Adding files.

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#java -jar forge-1.12.2-14.23.5.2854-installer.jar
+#mkdir ~/'Library/Application Support/minecraft/mods folder'
+mkdir -p ~/'Library/Application Support/minecraft/mods'
+
+cp mods/* ~/'Library/Application Support/minecraft/mods'
